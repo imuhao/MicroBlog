3214661db3a66e2d58d6661242d5091d3129a42a
smile <imuhao@qq.com>
2017-11-28 18:59:04 +0800

    initial commit

--- app/views.py
+++ app/views.py
@@ -29,9 +29,10 @@ def login():
     if form.is_submitted():
         flash("Login requested for OpenID=" + form.openid.data + ",remember_me=" + str(form.remember_me.data))
         return redirect('/index')
-    
+
     return render_template('login.html',
                            title="Sign In",
                            form=form,
                            providers=app.config["OPENID_PROVIDERS"]
                            )
+
