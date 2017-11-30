301d567003b8678c148d55e85a0e39194f865f83
smile <imuhao@qq.com>
2017-11-30 11:33:12 +0800

    register and login

--- app/forms.py
+++ app/forms.py
@@ -5,10 +5,15 @@
 # @Describe: 表单
 
 from flask_wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField,SubmitField
 from wtforms.validators import DataRequired
 
 
 class LoginFrom(Form):
-    openid = StringField('openid', validators=[DataRequired()])
+    # openid = StringField('openid', validators=[DataRequired()])
+
+    nickname = StringField('nickname', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+    sign_in = SubmitField('SignIn')
+    register = SubmitField('Register')
--- app/views.py
+++ app/views.py
@@ -14,9 +14,14 @@ from .models import User
 
 @app.route('/')
 @app.route('/index')
-@login_required
 def index():
+    nickname = session['nickname']
     user = g.user
+
+    # 没有登录
+    if user is None:
+        return redirect(url_for('login'))
+
     posts = [
         {
             'author': {'nickname': 'John'},
@@ -34,15 +39,51 @@ def index():
 
 
 @app.route("/login", methods=['GET', 'POST'])
-@oid.loginhandler
+# @oid.loginhandler
 def login():
+    # 自动登录
     if g.user is not None and g.user.is_authenticated:
         return redirect(url_for('index'))
-
     form = LoginFrom()
-    if form.validate_on_submit():
+
+    print(session['remember_me'])
+    print(session['nickname'])
+    if session['remember_me'] == True and not session['nickname'] == None:
+        return redirect(url_for('index'))
+
+    # 登录
+    if form.sign_in.data and form.is_submitted():
         session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+        nickname = form.nickname.data
+        password = form.password.data
+        user = User.query.filter_by(nickname=nickname).first()
+        if user == None:
+            flash("Your must fist register you nickname")
+            return redirect(url_for('login'))
+
+        if (not user.verify(password)):
+            flash("Your password is mistake")
+            return redirect(url_for('login'))
+
+        session['nickname'] = user.nickname
+        session['email'] = user.email
+        return redirect(url_for('index'))
+        # return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+
+    # 注册
+    if form.register.data and form.is_submitted():
+        nickname = form.nickname.data
+        password = form.password.data
+        user = User.query.filter_by(nickname=nickname).first()
+        if not user == None:
+            flash("User is Exist")
+            return redirect(url_for('login'))
+
+        u = User(nickname=nickname, password=password)
+        db.session.add(u)
+        db.session.commit()
+        flash("Register success")
+        return redirect(url_for('login'))
 
     return render_template('login.html',
                            title="Sign In",
@@ -51,43 +92,39 @@ def login():
                            )
 
 
-@oid.after_login
-def after_login(resp):
-    if resp.email is None or resp.email == "":
-        flash('Invalid login. Please try again.')
-        return redirect(url_for('login'))
-    user = User.query.filter_by(email=resp.email).first()
-    if user is None:
-        nickname = resp.nickname
-        if nickname is None or nickname == "":
-            nickname = resp.email.split('@')[0]
-        user = User(nickname=nickname, email=resp.email)
-        db.session.add(user)
-        db.session.commit()
-    remember_me = False
-    if 'remember_me' in session:
-        remember_me = session['remember_me']
-        session.pop('remember_me', None)
-    login_user(user, remember=remember_me)
-    return redirect(request.args.get('next') or url_for('index'))
-
+# @oid.after_login
+# def after_login(resp):
+#     if resp.email is None or resp.email == "":
+#         flash('Invalid login. Please try again.')
+#         return redirect(url_for('login'))
+#     user = User.query.filter_by(email=resp.email).first()
+#     if user is None:
+#         nickname = resp.nickname
+#         if nickname is None or nickname == "":
+#             nickname = resp.email.split('@')[0]
+#         user = User(nickname=nickname, email=resp.email)
+#         db.session.add(user)
+#         db.session.commit()
+#     remember_me = False
+#     if 'remember_me' in session:
+#         remember_me = session['remember_me']
+#         session.pop('remember_me', None)
+#     login_user(user, remember=remember_me)
+#     return redirect(request.args.get('next') or url_for('index'))
 
 # 退出登录
 @app.route("/logout")
 def logout():
-    logout_user()
+    session['nickname'] = None
+    g.user = None
     return redirect(url_for("index"))
-
-
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
 
 # 在每一个请求之前调用
 @app.before_request
 def before_request():
-    g.user = current_user
+    g.nickname = session['nickname']
+    g.user = User.query.filter_by(nickname=g.nickname).first()
 
 
 @app.route("/user/<nickname>")
