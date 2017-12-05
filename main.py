9958101f90e5610cd6d06d0f497fcf4ffcdeae25
smile <imuhao@qq.com>
2017-12-05 14:05:12 +0800

    add requirements.txt

--- main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2017/12/5 13:46
-# @Author  : Smile
-# @Describe:
-
-
-import app.run
--- run.py
+++ run.py
@@ -4,11 +4,5 @@
 # @Author  : Smile
 # @Describe: 运行 flask
 
-import sys
-from os import path
-sys.path.insert(0, path.pardir)
-
-print(sys.path)
 from app import app
-
 app.run(debug=True)
