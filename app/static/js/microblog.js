d58de2e0d32fafae96e0b82af12c81f719c80948
smile <imuhao@qq.com>
2017-12-11 11:45:50 +0800

    fall in love time

--- app/static/js/microblog.js
+++ app/static/js/microblog.js
@@ -18,11 +18,19 @@ function translate(post) {
 }
 
 
-$().ready(function () {
+function setUpTime() {
+    var startdate = new Date();  //开始时间
+    startdate.setFullYear(2017, 8, 3)
+    startdate.setHours(00, 00, 00)
+    var enddate = new Date();    //结束时间
+    var interval = enddate.getTime() - startdate.getTime()  //时间差的毫秒数
+    var days = Math.floor(interval / (24 * 3600 * 1000)) + 1
+    $('.menuRight').text('今天是我们相爱的第' + days + '天')
+    $('.menuRight').css('visibility', 'visible')
 
-})
-
-
-function selectIndex() {
-    $('#ulList li:nth-child(2)').addClass('active')
 }
+
+$(function () {
+    setUpTime()
+});
+
