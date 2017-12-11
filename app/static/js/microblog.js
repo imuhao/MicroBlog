8d888a60a5a2a682a6f51514b6b8f9cbe0d87fd7
smile <imuhao@qq.com>
2017-12-11 11:50:37 +0800

    fall in love time

--- app/static/js/microblog.js
+++ app/static/js/microblog.js
@@ -21,11 +21,13 @@ function translate(post) {
 function setUpTime() {
     var startdate = new Date();  //开始时间
     startdate.setFullYear(2017, 8, 3)
-    startdate.setHours(00, 00, 00)
+    startdate.setHours(21, 00, 00)
     var enddate = new Date();    //结束时间
     var interval = enddate.getTime() - startdate.getTime()  //时间差的毫秒数
-    var days = Math.floor(interval / (24 * 3600 * 1000)) + 1
-    $('.menuRight').text('今天是我们相爱的第' + days + '天')
+    var days = Math.floor(interval / (24 * 3600 * 1000)) + 1//天
+    var leave1 = interval % (24 * 3600 * 1000)    //计算天数后剩余的毫秒数
+    var hours = Math.floor(leave1 / (3600 * 1000)) + 1  //小时
+    $('.menuRight').text('今天是我们相爱的第' + days + '天' + hours + '小时')
     $('.menuRight').css('visibility', 'visible')
 
 }
